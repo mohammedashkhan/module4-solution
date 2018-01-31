7d8b8a452c10dcb9369afdd3a094fbe203edbe53
mohammedashkhan <30875725+mohammedashkhan@users.noreply.github.com>
2018-01-31 19:32:49 +0530

    Update and rename script1.js to speakHello.js

---
+++ speakHello.js
@@ -0,0 +1,37 @@
+var YaakovGreeter = {};
+var paulGreeter = {};
+var FrankGreeter = {};
+var LarryGreeter = {};
+var PaulaGreeter = {};
+var LauraGreeter = {};
+YaakovGreeter.name = "Yaakov";
+paulGreeter.name = "paul";
+FrankGreeter.name = "Frank";
+LarryGreeter.name = "Larry";
+PaulaGreeter.name = "Paula";
+LauraGreeter.name = "Laura";
+YaakovGreeter.sayHello = function () {
+console.log("Hello " + YaakovGreeter.name);
+}
+paulGreeter.sayHello = function () {
+console.log("Hello " + paulGreeter.name);
+}
+
+FrankGreeter.sayHello = function () {
+console.log("Hello " + FrankGreeter.name);
+}
+
+
+LarryGreeter.sayHello = function () {
+console.log("Hello " + LarryGreeter.name);
+}
+
+
+PaulaGreeter.sayHello = function () {
+console.log("Hello " + PaulaGreeter.name);
+}
+
+
+LauraGreeter.sayHello = function () {
+console.log("Hello " + LauraGreeter.name);
+}
