2cd731f766143278cd927fa85a21e85dbd544221
mohammedashkhan <30875725+mohammedashkhan@users.noreply.github.com>
2018-01-31 19:39:02 +0530

    Update and rename script2.js to SpeakGoodBoy.js

---
+++ SpeakGoodBoy.js
@@ -0,0 +1,28 @@
+var JohnGreeter = {};
+var JenGreeter = {};
+var JasonGreeter = {};
+var JimGreeter = {};
+
+JohnGreeter.name = "John";
+JenGreeter.name = "Jen";
+JasonGreeter.name = "Jason";
+JimGreeter.name = "Jim";
+
+JohnGreeter.sayGoodbye = function () {
+console.log("Goodbye " + JohnGreeter.name);
+}
+
+
+JenGreeter.sayGoodbye = function () {
+console.log("Goodbye " + JenGreeter.name);
+}
+
+
+JasonGreeter.sayGoodbye = function () {
+console.log("Goodbye " + JasonGreeter.name);
+}
+
+
+JimGreeter.sayGoodbye = function () {
+console.log("Goodbye " + JimGreeter.name);
+}
--- script2.js
+++
@@ -1,5 +0,0 @@
-var JohnGreeter = {};
-JohnGreeter.name = "John";
-JohnGreeter.sayGoodbye = function () {
-console.log("Goodbye " + JohnGreeter.name);
-}
