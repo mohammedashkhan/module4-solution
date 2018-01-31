f061cbac44c36d81b3426a1babce53def889235a
mohammedashkhan <30875725+mohammedashkhan@users.noreply.github.com>
2018-01-31 15:16:05 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,10 @@
 var sayeedGreeter = {};
+var paulGreeter = {};
 sayeedGreeter.name = "sayeed";
+paulGreeter.name = "paul";
 sayeedGreeter.sayHello = function () {
 console.log("Hello " + sayeedGreeter.name);
+}
+paulGreeter.sayHello = function () {
+console.log("Hello " + paulGreeter.name);
 }
